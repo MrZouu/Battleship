bec497e686cf0e4f4de55fb039d213a27a2b0dd1
MrZouu <lor3nsom@gmail.com>
2022-11-22 14:10:16 +0100

    Grille jeu

--- header.h
+++ header.h
@@ -4,8 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LIGNES 14
+#define COLONNES 14
+
 #ifndef BATTLESHIP_HEADER_H
 #define BATTLESHIP_HEADER_H
+
+typedef struct Plateau{
+    int grille[LIGNES][COLONNES];
+    int grilleAdverse[LIGNES][COLONNES];
+}t_plateau;
 
 typedef struct bateau{
     int quantite; //selon type
@@ -15,33 +23,32 @@ typedef struct bateau{
 }t_bateau;
 
 
-
 typedef struct PorteAvion{
     int symbole;        //Pas obliger vu que c'est une structure unique
     int numero;     //Numero du bateau ??
     int taille;
-    int postion;
+    int position;
 }t_PorteAv;
 
 typedef struct Croiseur{
     int symbole;        //Pas obliger vu que c'est une structure unique
     int numero;     //Numero du bateau ??
     int taille;
-    int postion;
+    int position;
 }t_Croiseur;
 
 typedef struct Destroyeur{
     int symbole;        //Pas obliger vu que c'est une structure unique
     int numero;     //Numero du bateau ??
     int taille;
-    int postion;
+    int position;
 }t_Destroy;
 
 typedef struct SousMarin{
     int symbole;        //Pas obliger vu que c'est une structure unique
     int numero;     //Numero du bateau ??
     int taille;
-    int postion;
+    int position;
 }t_SousMarin;
 
 
